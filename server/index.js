9704646c7ef2a34bb401400c7522a5dd52b3e9bf
Hiba Jabari <79831626+hiba99288@users.noreply.github.com>
2021-03-30 16:45:06 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+ const express = require('express')
 const app=express();
 const cors = require('cors')
 const mysql =require('mysql');
@@ -24,7 +24,6 @@ res.send("hibahiba");
 
 
 })
-
 
 
 app.get('/subareas',(req , res)=>{
@@ -96,10 +95,7 @@ res.send(false);
 }
 });
     
-})
-
-
-
+}) 
 
 app.get('/user',(req , res)=>{
   db.query('SELECT * FROM users;',
@@ -108,35 +104,19 @@ app.get('/user',(req , res)=>{
 
     res.send({err:err});
     
-} 
-
-
-
- 
+}  
 if (result.length>0 ){
      console.log(result);
     res.send(result);
      
-    
-
-    
   
 }else{
-res.send(false);
-
-
+res.send(false); 
 
 }
 });
     
-})
-
-
-
-
-
- 
-
+}) 
  //التحقق من اسم المستخدم و كلمة المرور
 app.post('/login',(req,res)=>{
 
@@ -152,24 +132,17 @@ app.post('/login',(req,res)=>{
 
     res.send({err:err});
     
-} 
-
-
-
- 
+}  
 if (result.length>0 ){
      console.log(result);
     res.send(result);
      
-    
-
-    
+   
   
 }else{
 res.send(false);
 
-
-
+ 
 }
 });
     
@@ -192,16 +165,17 @@ res.send(false);
 //اضافة مريض جديد
 app.post("/create", (req, res) => {
     const name = req.body.values.name;
+    const Idnumber= req.body.values.Idnumber;
     const phonenum = parseInt( req.body.values.phonenum);
     const sex = req.body.values.sex;
     const address = req.body.values.address;
-    const Checknumber = parseInt(req.body.values.Checknumber) ;
-    const  Checkresualt = req.body.values.Checkresualt;
     const email = req.body.values.email;
     const DOB = req.body.values.DOB;
+    const chk_date = req.body.values.chk_date;
+    const reason = req.body.values.reason;
     console.log(req.body);
-    db.query('INSERT INTO patient (  name, phonenum, sex, address,Checknumber,Checkresualt,email,DOB) VALUES (?,?,?,?,?,?,?,?)',
-        [name, phonenum, sex, address,Checknumber,Checkresualt,email,DOB],
+    db.query('INSERT INTO patient (  name,Idnumber, phonenum, sex, address,email,DOB,chk_date,reason) VALUES (?,?,?,?,?,?,?,?,?)',
+        [name, Idnumber,phonenum, sex, address,email,DOB ,chk_date,reason],
         (err, result) => {
           if (err) {
             console.log(err);
@@ -217,12 +191,12 @@ app.post("/create", (req, res) => {
       const idnumber =req.body.idnumber;
       const phonenum =req.body.phonenum;
       const name =req.body.neme;
-      const Checknumber = req.body.Checknumber;
-      const  Checkresualt = req.body.Checkresualt;
+      const covidTestResult = req.body.covidTestResult;
+      
   
       
       db.query(
-    "SELECT idnumber,phonenum ,name ,Checknumber,Checkresualt FROM patient WHERE idnumber=? AND phonenum=?   ",
+    "SELECT idnumber,phonenum ,name ,covidTestResult FROM patient NATURAL JOIN covidtest WHERE idnumber=? AND phonenum=?   ",
     [idnumber , phonenum  ],
     (err,result)=>{
       if(err){
@@ -251,51 +225,26 @@ app.post("/create", (req, res) => {
       
       });
 
+  // اضافة نتيجة
 
-
-      //المستشفى
-      app.post('/hos',(req,res)=>{
-
-        const id=req.body.id;
-        const H_name =req.body.H_name;
-        const H_place =req.body.H_place;
-        const Number_of_beds = req.body.Number_of_beds;
-        const Number_of_occupied_beds  = req.body.Number_of_occupied_beds ;
-        const Number_of_devices = req.body.Number_of_devices ;
-        const Number_of_occupied_devices  = req.body.Number_of_occupied_devices ;
-        
-       
-        
-        db.query(
-      "SELECT id,H_name ,H_place,Number_of_beds ,Number_of_occupied_beds,Number_of_devices,Number_of_occupied_devices FROM hospitals WHERE  id=?,H_name=? ,H_place=?,Number_of_beds=? ,Number_of_occupied_beds=?,Number_of_devices=?,Number_of_occupied_devices=?   ",
-      [id,H_name ,H_place,Number_of_beds ,Number_of_occupied_beds,Number_of_devices,Number_of_occupied_devices  ],
-      (err,result)=>{
-        if(err){
+  app.post("/tests", (req, res) => {
     
-        res.send({err:err});
-        
-    } 
-    
-    
-     
-    if (result.length>0 ){
-    
-        res.send(result);
-         
-        
-    
-        
-      
-    }else{
-    res.send({message:"wrong username/password"});
-    
-    
-    
-    }
-    });
-        
-        });
-
+    const covidTestResult= req.body.values.covidTestResult;
+   const CovidTestDate = req.body.values.CovidTestDate;
+    const Idnumber = req.body.values.Idnumber;
+   
+    console.log(req.body);
+    db.query('INSERT INTO covidtest ( covidTestResult ,CovidTestDate, Idnumber ) VALUES (?,?,? )',
+        [ covidTestResult,CovidTestDate , Idnumber],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.send("Values Inserted");
+          }
+        }
+      );
+    }); 
         
 //انشاء حساب مستخدم جديد
       app.post("/accounts", (req, res) => {
@@ -317,34 +266,13 @@ app.post("/create", (req, res) => {
             }
           );
         });
-    
-   
+ 
   app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'..','client','build','index.html'))
   })    
          
 
-  // app.get('/subareas', (req, res) => {
-  //   db.query('SELECT * FROM subareas;',
-  //   (err,result)=>{
-  //     if(err){
-  //     res.send({err:err});    
-  // } 
   
-  // if (result.length>0 ){
-  //     res.send(result);
-  // }else{
-  // res.send({message:"wrong username/password"});
-  // }
-  // })  });
-
-
-
-
-
-
-
-
 app.listen(2000 ,()=>{
 
 console.log("running in 2000");
@@ -352,61 +280,12 @@ console.log("running in 2000");
 
 })  
 
-
-//الدوائر
-app.post('/color',(req,res)=>{
-
-  const Case_id =req.body.Case_id;
-  const Case_type =req.body.Casep_type;
-  const cases_num =req.body.cases_num;
-  const case_color = req.body.case_color;
+ 
  
 
-  
-  db.query(
-"SELECT Case_id,Case_type ,cases_num ,case_color FROM cases WHERE Case_id =? AND Casep_type=? AND cases_num=?  AND case_color=?   ",
-[Case_id , Case_type,cases_num , case_color ],
-(err,result)=>{
-  if(err){
-
-  res.send({err:err});
-  
-} 
 
 
-
-if (result.length>0 ){
-
-  res.send(result);
    
-  
-
-  
-
-}else{
-res.send({message:"wrong username/password"});
-
-
-
-}
-});
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  });
-
 
 
   
