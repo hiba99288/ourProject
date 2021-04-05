1e85309ab82c0d2a512a12d860a156e0e46de5ad
Abdalrahim Fakhouri <abdilra7eem@gmail.com>
2021-04-05 12:47:38 +0300

    major improvements to addPat & Table

--- client/src/addPat.js
+++ client/src/addPat.js
@@ -76,7 +76,7 @@ function AddPat() {
 
   const deletePatient = (Idnumber) => {
     /* eslint-disable no-restricted-globals */
-    if (confirm('هل أنت متأكد؟')){
+    if (confirm('هل تريد حذف هذا السجل نهائياً؟')){
     /* eslint-enable no-restricted-globals */
       Axios.delete(`http://localhost:2000/delete/${Idnumber}`).then(
         (response) => {
@@ -125,13 +125,17 @@ function AddPat() {
 
   return (
     <div className="main">
+      <h2 class="main-title">المرضى</h2>
       {showAdd ? (
         <form onSubmit={addPatient}>
           <div class="button back-button"  onClick={() => setShowAdd(!showAdd)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 3.752l-4.423-3.752-7.771 9.039-7.647-9.008-4.159 4.278c2.285 2.885 5.284 5.903 8.362 8.708l-8.165 9.447 1.343 1.487c1.978-1.335 5.981-4.373 10.205-7.958 4.304 3.67 8.306 6.663 10.229 8.006l1.449-1.278-8.254-9.724c3.287-2.973 6.584-6.354 8.831-9.245z"/></svg>
+            {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 3.752l-4.423-3.752-7.771 9.039-7.647-9.008-4.159 4.278c2.285 2.885 5.284 5.903 8.362 8.708l-8.165 9.447 1.343 1.487c1.978-1.335 5.981-4.373 10.205-7.958 4.304 3.67 8.306 6.663 10.229 8.006l1.449-1.278-8.254-9.724c3.287-2.973 6.584-6.354 8.831-9.245z"/></svg> */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/></svg>
+            {/* <p>رجوع</p> */}
           </div>
+          <h3 class="sub-title">إضافة مريض</h3>
+          <hr class=".back-button-hr" />
           <br/>
-
           <div class="addPat-add-card">
             <label className="name">الاسم </label>
             <input
@@ -231,10 +235,19 @@ function AddPat() {
         <div>
           <div class="button add-button" onClick={() => setShowAdd(!showAdd)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7 14h-5v5h-4v-5h-5v-4h5v-5h4v5h5v4z"/></svg>
+            <p>إضافة مريض</p>
           </div>
-          <div class="button edit-button" onClick={() => setShow(!show)}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/></svg>
-          </div>
+          {show ? (
+              <div class="button edit-button" onClick={() => setShow(!show)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/></svg>
+                <p>تفعيل وضع التعديل</p>
+              </div>
+            ) : (
+              <div class="button edit-button edit-button-disable" onClick={() => setShow(!show)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/></svg>
+                <p>تعطيل وضع التعديل</p>
+              </div>
+          )}
           <table>
             <thead>
               <tr>
