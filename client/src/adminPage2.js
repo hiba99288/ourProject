3ba3a02f7c6088337e47b3a6e4a482a47b80f625
Abdalrahim Fakhouri <abdilra7eem@gmail.com>
2021-04-10 18:19:27 +0300

    auth: not-auth admin page go back

--- client/src/adminPage2.js
+++ client/src/adminPage2.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from 'react-router-dom';
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
@@ -12,6 +13,13 @@ import MapSetting from "./mapSetting";
 import UpdateResult from "./updateResult";
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
+  const history = useHistory();
+  useEffect(()=>{
+    if(localStorage.getItem('account_type') !== "admin"){
+      history.goBack();
+    }
+  });
+
   return (
     <div
       role="tabpanel"
