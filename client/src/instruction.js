ec0def5c118a211d7c35595d2bce307bb519051c
Abdalrahim Fakhouri <abdilra7eem@gmail.com>
2021-04-11 00:58:57 +0300

    clear up instruction page

--- client/src/instruction.js
+++ client/src/instruction.js
@@ -1,54 +1,29 @@
-import React, {Component} from 'react';
-import axios from 'axios'
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
-import "./styles.css";
-export default class Instruction extends Component {
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
 
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      galleryItems: [],
-    };
-}
- getData (){
-    axios.get(`http://localhost:2000/api/instructions`, {})
-        .then(res => {
-                const data = res.data
-              const p = data.map(m => 
-             <h3>    {m.Instruction_text} </h3> 
-              )
-              this.setState({
-                galleryItems: p
-              })
-            }).catch((error) => {
-                console.log(error)
-            })
-  }
-  responsive = {
-    0: { items: 1 },
-    1024: { items: 2 },
-  }
-  componentDidMount() {
-   this.getData()
-}
-  
-  render() {
-    return (
-      <div style={{backgroundColor:"blue" ,height:"50px" ,color:"white"  }}>
-        <AliceCarousel
-        
-        items={this.state.galleryItems}
-        responsive={this.responsive}
-        autoPlayInterval={1500}
-        autoPlayDirection="ltr"
-        autoPlay={true}
-        infinite ={true}
-        fadeOutAnimation={true}
-        mouseTrackingEnabled={true}
-        disableAutoPlayOnAction={true}
-      />
-      </div>
-    )
-  }
+export default function Instruction(props) {
+  const [galleryItems, setGalleryItems] = useState({});
+  useEffect(()=>{
+    const getData = () => {
+      axios
+        .get(`http://localhost:2000/api/instructions`, {})
+        .then((res) => {
+          const data = res.data;
+          const p = data.map((m) => <h3> {m.Instruction_text} </h3>);
+          setGalleryItems({
+            galleryItems: p,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    getData();
+  });
+
+  return (
+    <div >
+
+    </div>
+  );
 }
