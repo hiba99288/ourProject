b84febf6860d613a940f1434b56e1b5869241f46
Abdalrahim Fakhouri <abdilra7eem@gmail.com>
2021-04-11 00:56:58 +0300

    rework: updateResult

--- client/src/updateResult.js
+++ client/src/updateResult.js
@@ -1,48 +1,71 @@
-import React, { useEffect, useState   } from "react";
-import Axios from 'axios';
-import {useHistory} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
+import './Table.css';
+
 function UpdateResult() {
-
-
-  const [values,setValues]= useState({
-    CovidTestDate :null,
-    covidTestResult:null,
-    Idnumber:null,
-    
-  })
+  const [values, setValues] = useState({
+    CovidTestDate: null,
+    covidTestResult: null,
+    Idnumber: null,
+  });
   const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });};
-   const History=useHistory();
-   const addTest =(e) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const History = useHistory();
+  const addTest = (e) => {
     e.preventDefault();
-    Axios.post('http://localhost:2000/tests',{
-            
-      values
-}).then(()=>{
+    Axios.post("http://localhost:2000/tests", {
+      values,
+    }).then(() => {
+      console.log("success");
 
-console.log("success");
- 
-History.push('/adminPage2');
-})
-}; 
+      History.push("/adminPage2");
+    });
+  };
 
-
-    return ( 
+  return (
     <div>
       <form onSubmit={addTest}>
-      <h2 className="name">رقم الهوية</h2><br></br><br></br>
-      <input className="firstname" type="number"  onChange={handleChange('Idnumber')}  /> 
+        <h3 class="sub-title">تعديل نتيجة فحص</h3>
+        <hr class=".back-button-hr"></hr>
+        <br/>
 
-       <h2 className="name">نتيجة الفحص</h2><br></br><br></br>
-  <select    className="option"  onChange={handleChange('covidTestResult')}>
-  
-              <option>P</option>
-              <option>N</option>
-        </select>
-        
-        <h2 className="name">تاريخ النتيجة</h2><br></br><br></br>
-    <input className="firstname" type="date"  onChange={handleChange('CovidTestDate')} />
- <br></br>
-      <button>تسجيل النتيجة </button>
-      </form></div> )} 
-      export default UpdateResult
+        <div class="addPat-add-card">
+          <label class="name">رقم الهوية </label>
+          <input
+            className="firstname"
+            type="number"
+            onChange={handleChange("Idnumber")}
+          />
+        </div>
+
+        <div class="addPat-add-card">
+          <label class="name">نتيجة الفحص </label>
+          <input
+            className="firstname"
+            type="number"
+            onChange={handleChange("Idnumber")}
+          />
+        </div>
+
+        <div class="addPat-add-card">
+          <label class="name">نتيجة الفحص</label>
+          <select class="option" onChange={handleChange("covidTestResult")}>
+            <option>P</option>
+            <option>N</option>
+          </select>
+        </div>
+
+        <div class="addPat-add-card">
+          <label class="name">تاريخ النتيجة</label>
+          <input class="firstname" type="date" onChange={handleChange("CovidTestDate")} />
+        </div>
+
+        <br/>
+        <div class="button addbtn">اضافة</div>
+      </form>
+    </div>
+  );
+}
+export default UpdateResult;
